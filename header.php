83482233998ccb7d9294ba195b62e67fd11444b4
Travis Berry <travisberry@travisberry.com>
2009-11-19 16:32:51 -0700

    Changes css to have @fontface declared first. Added class=chunk to html tag in header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html class="chunk">
 <head profile="http://gmpg.org/xfn/11">
 <title><?php bloginfo('name'); ?> &middot; <?php bloginfo('description'); ?></title>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
