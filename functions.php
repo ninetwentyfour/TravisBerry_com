475dd51d9dd9c85977da02a8910b069e57e3e828
Travis Berry <travisberry@travisberry.com>
2009-11-20 15:44:27 -0700

    Added to functions.php to remove the jump in the "read more" link. Now loads at top of the page.

--- functions.php
+++ functions.php
@@ -9,4 +9,17 @@ add_action( 'wp_print_styles', 'deregister_ct7_styles', 100 );
 function deregister_ct7_styles() {
 wp_deregister_style( 'contact-form-7' );
 }
+
+
+function remove_more_jump_link($link) { 
+$offset = strpos($link, '#more-');
+if ($offset) {
+$end = strpos($link, '"',$offset);
+}
+if ($end) {
+$link = substr_replace($link, '', $offset, $end-$offset);
+}
+return $link;
+}
+add_filter('the_content_more_link', 'remove_more_jump_link');
 ?>
