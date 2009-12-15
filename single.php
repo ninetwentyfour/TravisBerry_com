f18298357e0b8482abf9aff7b87af89ab2f36621
Travis Berry <travisberry@travisberry.com>
2009-12-14 20:05:58 -0700

    Randomized More post links

--- single.php
+++ single.php
@@ -16,7 +16,7 @@
 
 	
 	<?php comments_template(); ?>
-
+<div id="moreposts1">
 <?php
 $categories = get_the_category($post->ID);
 if ($categories) {
@@ -26,10 +26,11 @@ if ($categories) {
 	$args=array(
 		'category__in' => $category_ids,
 		'post__not_in' => array($post->ID),
+		'orderby' => 'rand',
 		'showposts'=>3, // Number of related posts that will be shown.
 		'caller_get_posts'=>1
 	);
-	$my_query = new wp_query($args);
+$my_query = new wp_query($args);
 	if( $my_query->have_posts() ) {
 		echo '<h3>Read More Posts</h3><ul>';
 		while ($my_query->have_posts()) {
@@ -42,7 +43,7 @@ if ($categories) {
 	}
 }
 ?>
-
+</div>
 	<?php endwhile; else: ?>
 			 
 
