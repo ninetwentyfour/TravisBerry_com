d863ad9f0eefdbc138ac3f40725582fa8635f1ec
Travis Berry <travisberry@travisberry.com>
2009-12-14 19:36:35 -0700

    Added more posts list to end of blog posts

--- single.php
+++ single.php
@@ -17,12 +17,37 @@
 	
 	<?php comments_template(); ?>
 
+<?php
+$categories = get_the_category($post->ID);
+if ($categories) {
+	$category_ids = array();
+	foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+
+	$args=array(
+		'category__in' => $category_ids,
+		'post__not_in' => array($post->ID),
+		'showposts'=>3, // Number of related posts that will be shown.
+		'caller_get_posts'=>1
+	);
+	$my_query = new wp_query($args);
+	if( $my_query->have_posts() ) {
+		echo '<h3>Read More Posts</h3><ul>';
+		while ($my_query->have_posts()) {
+			$my_query->the_post();
+		?>
+			<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+		<?php
+		}
+		echo '</ul>';
+	}
+}
+?>
+
 	<?php endwhile; else: ?>
-			  <div id="next"><?php next_posts_link('Older posts') ?></div><div id="previous"><?php previous_posts_link('Newer posts') ?></div>
-		<p>Sorry, no posts matched your criteria.</p>
+			 
 
 <?php endif; ?>
-<a href="http://www.travisberry.com/blog/">back to blog</a>
+<p><a href="http://www.travisberry.com/blog/">back to blog</a></p>
 	</div>
 
 </div>
