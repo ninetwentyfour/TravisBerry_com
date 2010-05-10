50a72b2c0b8ff393e3da16d6aae0bcbd20cd5bad
Travis Berry <travisberry@travisberry.com>
2010-05-09 19:36:57 -0600

    added image swap javascript for use on contact page

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 	<img src="http://www.travisberry.com/wp-content/themes/tbsite/images/footerimage.jpg" width="388" height="184" alt="Travis Berry" title="Travis Berry" />
 		</div></footer></div><?php wp_footer(); ?>
 <script type="text/javascript" src="http://m.travisberry.com/mobify/redirect.js"></script>
+<script type="text/javascript" src="http://www.travisberry.com/wp-content/themes/tbsite/imageswap.js"></script>
 <script type="text/javascript">try{_mobify("http://m.travisberry.com/");} catch(err) {};</script>
 <!--[if lt IE 8]>
 <script src="http://ie7-js.googlecode.com/svn/version/2.0(beta3)/IE8.js" type="text/javascript"></script>
